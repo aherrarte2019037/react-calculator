9cebec5838d83512f0eb27da2891868d154f054d
Angel Herrarte <angelherrarte3@gmail.com>
2024-08-21 21:22:02 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,19 @@ function App() {
 
   return (
     <div className="App w-100 h-100">
-      <Calculator />
+      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-1.png" alt="Ejercicio 1" border="0">
+      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-2.png" alt="Ejercicio 2" border="0">
+      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-3.png" alt="Ejercicio 3" border="0">
+      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-4.jpg" alt="Ejercicio 4" border="0">
+      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-5.jpg" alt="Ejercicio 5" border="0">
+      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-6.jpg" alt="Ejercicio 6" border="0">
+      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-7.png" alt="Ejercicio 7" border="0">
+      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-8.jpg" alt="Ejercicio 8" border="0">
+      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-10-y-11.png" alt="Ejercicio 10 y 11" border="0">
+      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-12.png" alt="Ejercicio 12" border="0">
+      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-13.png" alt="Ejercicio 13" border="0">
+      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-14.png" alt="Ejercicio 14" border="0">
+      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-15.png" alt="Ejercicio 15" border="0">
     </div>
   );
 }
