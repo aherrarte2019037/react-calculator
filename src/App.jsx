55b765e2dc12efd0e9ae924a25f24860dab32fd8
Angel <angelherrarte3@gmail.com>
2024-08-21 22:08:37 -0600

    Refactor image sources in App.jsx to use relative paths

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,20 @@ function App() {
 
   return (
     <div className="w-100 h-100" style={{ width: '100svw', height : '100svh', display: 'flex', flexDirection: 'column', gap: '50px' }}>
-      <img src="../src/assets/Ejercicio 1.png" border="0"/>
-      <img src="../src/assets/Ejercicio 2.png" border="0"/>
-      <img src="../src/assets/Ejercicio 3.png" border="0"/>
-      <img src="../src/assets/Ejercicio 4.png" border="0"/>
-      <img src="../src/assets/Ejercicio 5.png" border="0"/>
-      <img src="../src/assets/Ejercicio 6.png" border="0"/>
-      <img src="../src/assets/Ejercicio 7.png" border="0"/>
-      <img src="../src/assets/Ejercicio 8.png" border="0"/>
-      <img src="../src/assets/Ejercicio 9.jpg" border="0"/>
-      <img src="../src/assets/Ejercicio 10 y 11.jpg" border="0"/>
-      <img src="../src/assets/Ejercicio 12.jpg" border="0"/>
-      <img src="../src/assets/Ejercicio 13.jpg" border="0"/>
-      <img src="../src/assets/Ejercicio 14.jpg" border="0"/>
-      <img src="../src/assets/Ejercicio 15.jpg" border="0"/>
+      <img src={"/static/images/Ejercicio 1.png"} border="0"/>
+      <img src={"/static/images/Ejercicio 2.png"} border="0"/>
+      <img src={"/static/images/Ejercicio 3.png"} border="0"/>
+      <img src={"/static/images/Ejercicio 4.png"} border="0"/>
+      <img src={"/static/images/Ejercicio 5.png"} border="0"/>
+      <img src={"/static/images/Ejercicio 6.png"} border="0"/>
+      <img src={"/static/images/Ejercicio 7.png"} border="0"/>
+      <img src={"/static/images/Ejercicio 8.png"} border="0"/>
+      <img src={"/static/images/Ejercicio 9.jpg"} border="0"/>
+      <img src={"/static/images/Ejercicio 10 y 11.jpg"} border="0"/>
+      <img src={"/static/images/Ejercicio 12.jpg"} border="0"/>
+      <img src={"/static/images/Ejercicio 13.jpg"} border="0"/>
+      <img src={"/static/images/Ejercicio 14.jpg"} border="0"/>
+      <img src={"/static/images/Ejercicio 15.jpg"} border="0"/>
     </div>
   );
 }
