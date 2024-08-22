1d2a1798278e5c87890ac86060714598f8d8f583
Angel <angelherrarte3@gmail.com>
2024-08-21 22:03:15 -0600

    Refactor App.jsx to use relative paths for image sources

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,21 @@ import Calculator from './Calculator/Calculator';
 function App() {
 
   return (
-    <div className="w-100 h-100" style={{ width: '100svw', height : '100svh' }}>
-      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-1.png" alt="Ejercicio 1" border="0"/>
-      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-2.png" alt="Ejercicio 2" border="0"/>
-      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-3.png" alt="Ejercicio 3" border="0"/>
-      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-4.jpg" alt="Ejercicio 4" border="0"/>
-      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-5.jpg" alt="Ejercicio 5" border="0"/>
-      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-6.jpg" alt="Ejercicio 6" border="0"/>
-      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-7.png" alt="Ejercicio 7" border="0"/>
-      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-8.jpg" alt="Ejercicio 8" border="0"/>
-      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-10-y-11.png" alt="Ejercicio 10 y 11" border="0"/>
-      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-12.png" alt="Ejercicio 12" border="0"/>
-      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-13.png" alt="Ejercicio 13" border="0"/>
-      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-14.png" alt="Ejercicio 14" border="0"/>
-      <img src="https://tinypic.host/images/2024/08/22/Ejercicio-15.png" alt="Ejercicio 15" border="0"/>
+    <div className="w-100 h-100" style={{ width: '100svw', height : '100svh', display: 'flex', flexDirection: 'column', gap: '50px' }}>
+      <img src="../src/assets/Ejercicio 1.png" border="0"/>
+      <img src="../src/assets/Ejercicio 2.png" border="0"/>
+      <img src="../src/assets/Ejercicio 3.png" border="0"/>
+      <img src="../src/assets/Ejercicio 4.png" border="0"/>
+      <img src="../src/assets/Ejercicio 5.png" border="0"/>
+      <img src="../src/assets/Ejercicio 6.png" border="0"/>
+      <img src="../src/assets/Ejercicio 7.png" border="0"/>
+      <img src="../src/assets/Ejercicio 8.png" border="0"/>
+      <img src="../src/assets/Ejercicio 9.jpg" border="0"/>
+      <img src="../src/assets/Ejercicio 10 y 11.jpg" border="0"/>
+      <img src="../src/assets/Ejercicio 12.jpg" border="0"/>
+      <img src="../src/assets/Ejercicio 13.jpg" border="0"/>
+      <img src="../src/assets/Ejercicio 14.jpg" border="0"/>
+      <img src="../src/assets/Ejercicio 15.jpg" border="0"/>
     </div>
   );
 }
