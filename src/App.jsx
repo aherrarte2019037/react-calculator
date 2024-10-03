888f1a5e92554d8eb89a8c7c61bf767b1d78f5bb
Angel Herrarte <angelherrarte3@gmail.com>
2024-10-02 20:08:13 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,30 @@ function App() {
 
   return (
     <div className="w-100 h-100" style={{ width: '100svw', height : '100svh', display: 'flex', flexDirection: 'column', gap: '50px' }}>
-      <img src={"/static/images/Ejercicio 1.png"} border="0"/>
-      <img src={"/static/images/Ejercicio 2.png"} border="0"/>
-      <img src={"/static/images/Ejercicio 3.png"} border="0"/>
-      <img src={"/static/images/Ejercicio 4.png"} border="0"/>
-      <img src={"/static/images/Ejercicio 5.png"} border="0"/>
-      <img src={"/static/images/Ejercicio 6.png"} border="0"/>
-      <img src={"/static/images/Ejercicio 7.png"} border="0"/>
-      <img src={"/static/images/Ejercicio 8.png"} border="0"/>
-      <img src={"/static/images/Ejercicio 9.jpg"} border="0"/>
-      <img src={"/static/images/Ejercicio 10 y 11.jpg"} border="0"/>
-      <img src={"/static/images/Ejercicio 12.jpg"} border="0"/>
-      <img src={"/static/images/Ejercicio 13.jpg"} border="0"/>
-      <img src={"/static/images/Ejercicio 14.jpg"} border="0"/>
-      <img src={"/static/images/Ejercicio 15.jpg"} border="0"/>
+      <img src={"/static/images/1.jpg"} border="0"/>
+      <img src={"/static/images/2.jpg"} border="0"/>
+      <img src={"/static/images/3.jpg"} border="0"/>
+      <img src={"/static/images/4.jpg"} border="0"/>
+      <img src={"/static/images/5.jpg"} border="0"/>
+      <img src={"/static/images/6.jpg"} border="0"/>
+      <img src={"/static/images/7.jpg"} border="0"/>
+      <img src={"/static/images/8.jpg"} border="0"/>
+      <img src={"/static/images/9.jpg"} border="0"/>
+      <img src={"/static/images/10.jpg"} border="0"/>
+      <img src={"/static/images/11.jpg"} border="0"/>
+      <img src={"/static/images/12.jpg"} border="0"/>
+      <img src={"/static/images/13.jpg"} border="0"/>
+      <img src={"/static/images/14.jpg"} border="0"/>
+      <img src={"/static/images/15.jpg"} border="0"/>
+      <img src={"/static/images/16.jpg"} border="0"/>
+      <img src={"/static/images/17.jpg"} border="0"/>
+      <img src={"/static/images/18.jpg"} border="0"/>
+      <img src={"/static/images/19.jpg"} border="0"/>
+      <img src={"/static/images/20.jpg"} border="0"/>
+      <img src={"/static/images/21.jpg"} border="0"/>
+      <img src={"/static/images/22.jpg"} border="0"/>
+      <img src={"/static/images/23.jpg"} border="0"/>
+      <img src={"/static/images/24.jpg"} border="0"/>
     </div>
   );
 }
