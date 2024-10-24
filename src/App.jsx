9fe97668ad2150a035dfb0f8e9156ef052d75018
Angel <angelherrarte3@gmail.com>
2024-10-24 06:28:26 -0600

    Remove unused test.txt and update image sources in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,10 @@ function App() {
 
   return (
     <div className="w-100 h-100" style={{ width: '100svw', height : '100svh', display: 'flex', flexDirection: 'column', gap: '50px' }}>
-      <img src={"/static/images/x.jpeg"} border="0"/>
-      <img src={"/static/images/y.jpeg"} border="0"/>
-      <img src={"/static/images/z.jpeg"} border="0"/>
-      <img src={"/static/images/1.jpg"} border="0"/>
-      <img src={"/static/images/2.jpg"} border="0"/>
-      <img src={"/static/images/3.jpg"} border="0"/>
-      <img src={"/static/images/4.jpg"} border="0"/>
-      <img src={"/static/images/5.jpg"} border="0"/>
-      <img src={"/static/images/6.jpg"} border="0"/>
-      <img src={"/static/images/7.jpg"} border="0"/>
-      <img src={"/static/images/8.jpg"} border="0"/>
-      <img src={"/static/images/9.jpg"} border="0"/>
-      <img src={"/static/images/10.jpg"} border="0"/>
-      <img src={"/static/images/11.jpg"} border="0"/>
-      <img src={"/static/images/12.jpg"} border="0"/>
-      <img src={"/static/images/13.jpg"} border="0"/>
-      <img src={"/static/images/14.jpg"} border="0"/>
-      <img src={"/static/images/15.jpg"} border="0"/>
-      <img src={"/static/images/16.jpg"} border="0"/>
-      <img src={"/static/images/17.jpg"} border="0"/>
-      <img src={"/static/images/18.jpg"} border="0"/>
-      <img src={"/static/images/19.jpg"} border="0"/>
-      <img src={"/static/images/20.jpg"} border="0"/>
-      <img src={"/static/images/21.jpg"} border="0"/>
-      <img src={"/static/images/22.jpg"} border="0"/>
-      <img src={"/static/images/23.jpg"} border="0"/>
-      <img src={"/static/images/24.jpg"} border="0"/>
+      <img src={"/static/images/1.png"} border="0"/>
+      <img src={"/static/images/2.png"} border="0"/>
+      <img src={"/static/images/3.png"} border="0"/>
+      <img src={"/static/images/4.png"} border="0"/>
     </div>
   );
 }
